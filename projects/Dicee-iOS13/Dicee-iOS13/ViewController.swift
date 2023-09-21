45fd94b626de170bc0b347698c788bafcc13d634
Andrew Zhang <71068270+azk1r1n@users.noreply.github.com>
2023-09-21 00:46:25 -0700

    Add ignore file.

---
+++ projects/Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // IBOutlet allows me to reference a UI element.
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        diceImageView1.image = UIImage(named: "DiceSix")
+        diceImageView2.image = UIImage(named: "DiceTwo")
+        // Challenge from Angela diceImageView1.alpha = 0.5
+        
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        // print("Button got tapped.")
+        diceImageView1.image = UIImage(named: "DiceFour")
+        diceImageView2.image = UIImage(named: "DiceFour")
+    }
+    
+}
+
