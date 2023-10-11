0dd3ef2fd99f1f515b39a506fbd28046954c2938
Andrew Zhang <71068270+azk1r1n@users.noreply.github.com>
2023-10-11 00:06:11 -0700

    Updates based on Section 3: 36-38.

--- projects/Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
+++ projects/Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // IBOutlet allows me to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        diceImageView1.image = UIImage(named: "DiceSix")
+        diceImageView1.alpha = 0.5
+        
+        diceImageView1.image = UIImage(named: "DiceTwo")
+        diceImageView1.alpha = 0.5
+        
     }
 
 
