9b19313077ab922e0f083dbac403e4d41ecc8ca0
Andrew Zhang <71068270+azk1r1n@users.noreply.github.com>
2023-11-01 23:56:14 -0700

    Half through Section 71 with the help of GPT.

--- projects/Xylophone-iOS13/Xylophone/ViewController.swift
+++ projects/Xylophone-iOS13/Xylophone/ViewController.swift
@@ -7,15 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
+    var audioPlayer: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Get the URL of the sound file
+        if let soundURL = Bundle.main.url(forResource: "C", withExtension: "wav") {
+            do {
+                // Initialize the audio player with the sound file URL
+                audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+            } catch {
+                print("Error loading sound file: \(error.localizedDescription)")
+            }
+        } else {
+            print("Sound file not found in the Sounds directory.")
+        }
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        
+        // Check if the audio player is not nil
+        if let player = audioPlayer {
+            // Stop the currently playing sound
+            if player.isPlaying {
+                player.stop()
+            }
+            
+            // Prepare and play the new sound
+            player.currentTime = 0  // Reset the playback position to the beginning
+            player.prepareToPlay()
+            player.play()
+        }
     }
     
 
