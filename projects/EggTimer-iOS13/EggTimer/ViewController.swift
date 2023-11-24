3ca886c6b602e5044e1e251ad827b6936f4b3061
Andrew Zhang <71068270+azk1r1n@users.noreply.github.com>
2023-11-24 01:40:39 -0800

    Minor updates.

--- projects/EggTimer-iOS13/EggTimer/ViewController.swift
+++ projects/EggTimer-iOS13/EggTimer/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        print(sender.currentTitle ?? "")
+        
+        
+    }
+    
 }
